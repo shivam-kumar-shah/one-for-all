ee40dc024e159a391511b84fb25f4d3bdebcc67a
Shivam Kumar <shivam-kumar-shah@outlook.com>
2023-10-08 10:28:01 +0530

    fix: :ambulance: add cors

--- dist/app.js
+++ dist/app.js
@@ -5,12 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const database_1 = require("./config/database");
 const auth_1 = __importDefault(require("./routes/auth"));
 const kanban_1 = __importDefault(require("./routes/kanban"));
 const error_1 = require("./config/error");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.use("/auth", auth_1.default);
 app.use("/kanban", kanban_1.default);
 app.use(error_1.errorHandler);
